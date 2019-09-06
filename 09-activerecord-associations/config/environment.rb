07126a7190afd48e9139fb7ac95ed6e4e6e89512
dahby <dstoll243@gmail.com>
2019-09-06 07:47:15 -0700

    prepped for ar/assoc lecture

---
+++ 09-activerecord-associations/config/environment.rb
@@ -0,0 +1,11 @@
+require 'bundler'
+Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/development.sqlite"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
